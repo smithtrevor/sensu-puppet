b8c912d3e33b9dfcda20a6f3903d8f2fdc875333
Trevor Smith <tsmith3@entertainment.com>
2016-07-07 17:10:03 -0400

    add check for sensu::enterprise to prevent running the dashboard class on things that don't need it

--- manifests/enterprise/dashboard.pp
+++ manifests/enterprise/dashboard.pp
@@ -3,10 +3,12 @@
 # Installs the Sensu Enterprise Dashboard
 class sensu::enterprise::dashboard {
 
-  anchor { 'sensu::enterprise::dashboard::begin': } ->
-  class { '::sensu::enterprise::dashboard::package': } ->
-  class { '::sensu::enterprise::dashboard::config': } ->
-  class { '::sensu::enterprise::dashboard::service': } ->
-  anchor { 'sensu::enterprise::dashboard::end': }
+  if $::sensu::enterprise {
+    anchor { 'sensu::enterprise::dashboard::begin': } ->
+    class { '::sensu::enterprise::dashboard::package': } ->
+    class { '::sensu::enterprise::dashboard::config': } ->
+    class { '::sensu::enterprise::dashboard::service': } ->
+    anchor { 'sensu::enterprise::dashboard::end': }
+  }
 
 }
